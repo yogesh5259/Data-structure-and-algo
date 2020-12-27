4f6206fa33f806de0b01451aa3802638442d83fa
MacBook <patelyogesh5040@gmail.com>
2020-12-27 19:32:51 +0530

    Quick union

--- Algorithms/QuickUnion/QuickUnion.java
+++ Algorithms/QuickUnion/QuickUnion.java
@@ -1,4 +1,37 @@
 package Com.Algorithms.QuickUnion;
 
 public class QuickUnion {
+
+    private int[] id;
+    private int count;
+
+    public QuickUnion(int n){
+        count = n;
+        id = new int[n];
+        for (int i = 0; i < n; i++) {
+            id[i] = i;
+        }
+    }
+
+    public int getCount(){
+        return count;
+    }
+
+    private int root(int i){
+        while (i != id[i]) i = id[i];
+        return i;
+    }
+
+    public boolean connected(int p, int q){
+        return root(p) == root(q);
+    }
+
+    public void union(int p, int q){
+        int i = root(p);
+        int j = root(q);
+
+        id[i] = j;
+        count--;
+    }
+
 }
--- Algorithms/QuickUnion/mainApp.java
+++ Algorithms/QuickUnion/mainApp.java
@@ -1,5 +1,19 @@
 package Com.Algorithms.QuickUnion;
 
+import java.util.Scanner;
+
 public class mainApp {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        QuickUnion qf = new QuickUnion(n);
+        while (!scanner.hasNext("q")){
+            int p = scanner.nextInt();
+            int q = scanner.nextInt();
+            if(qf.connected(p,q))continue;
+            qf.union(p,q);
+            System.out.println(p+ "..." +q);
+        }
+        System.out.println(qf.getCount());
+    }
 }
