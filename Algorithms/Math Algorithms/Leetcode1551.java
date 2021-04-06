10d61aad102a92da3954eb42205ee53cfb59c3bb
MacBook <patelyogesh5040@gmail.com>
2021-04-06 17:40:43 +0530

    Leetcode1551

---
+++ Algorithms/Math Algorithms/Leetcode1551.java
@@ -0,0 +1,8 @@
+public class Leetcode1551 {
+    public static int minOperations(int n) {
+        return n*n / 4;
+    }
+    public static void main(String[] args) {
+        System.out.println(minOperations(6));
+    }
+}
