df19e5c74bfd2610f694029a6e5ec6b4580b3b76
MacBook <patelyogesh5040@gmail.com>
2021-04-12 19:55:53 +0530

    Leetcode42 optimal

--- Arrays/Problems On Arrays/Leetcode42.java
+++ Arrays/Problems On Arrays/Leetcode42.java
@@ -1,32 +1,26 @@
-package Leetcode;
-
-public class Leetcode42 {
-    public static int trap(int[] height) {
-        int totalWater = 0;
-        for (int i = 0; i < height.length; i++) {
-            int currentWater = 0;
-            int leftMax = 0;
-            int rightMax = 0;
-            int left = i,right = i;
-            while(left >= 0){
-                leftMax = Math.max(leftMax,height[left]);
-                left--;
-            }
-            while(right < height.length){
-                rightMax = Math.max(rightMax, height[right]);
-                right++;
-            }
-
-            currentWater = Math.min(leftMax, rightMax) - height[i];
-            System.out.println(currentWater);
-            if (currentWater >= 0){
-                totalWater += currentWater;
-            }
-        }
-        return totalWater;
-    }
+public class Leetcode42{
+    public static int trap(int[] heights) {
+        int maxLeft = 0;
+         int left = 0;
+         int right = heights.length - 1;
+         int maxRight = 0;
+         int maxValue = 0;
+         int i = 0;
+         while(left < right){
+             if(heights[left] <= heights[right]){
+                 if(maxLeft <= heights[left]) maxLeft = heights[left];
+                 maxValue += maxLeft - heights[left];
+                 left++;
+             } else{
+                 if (maxRight <= heights[right]) maxRight = heights[right];
+                 maxValue += maxRight - heights[right];
+                 right--;
+             }
+         }
+         return maxValue;
+     }
     public static void main(String[] args) {
-        int[] input =  {0,1,0,2,1,0,1,3,2,1,2,1};
-        System.out.println();
+        int[] input = {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(trap(input));
     }
 }
