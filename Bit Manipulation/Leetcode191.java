e102b7d62abef372239536333564ab66d924268e
MacBook <patelyogesh5040@gmail.com>
2021-04-25 19:37:07 +0530

    Leetcode191

---
+++ Bit Manipulation/Leetcode191.java
@@ -0,0 +1,15 @@
+public class Leetcode191 {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count = 0;
+
+        for (int i = 0; i < 32; i++) {
+            int mask = 1 << i;
+            if ((n & mask) != 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
