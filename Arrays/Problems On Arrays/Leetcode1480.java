bb147c3740e87de565a5c41a293c3f31d3ba4e16
MacBook <patelyogesh5040@gmail.com>
2021-05-03 18:48:42 +0530

    leetcode1480

---
+++ Arrays/Problems On Arrays/Leetcode1480.java
@@ -0,0 +1,8 @@
+public class Leetcode1480 {
+    public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+}
