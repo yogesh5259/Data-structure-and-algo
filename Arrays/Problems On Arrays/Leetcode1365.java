ba2e7b4cdfe78ea6605e8e2ef3ea08c04a79b272
MacBook <patelyogesh5040@gmail.com>
2021-05-16 20:26:25 +0530

    Leetcode1365

---
+++ Arrays/Problems On Arrays/Leetcode1365.java
@@ -0,0 +1,15 @@
+public class Leetcode1365{
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] ans = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 0; j < nums.length; j++){
+                if(nums[i] > nums[j]){
+                    ans[i]++;
+                }
+            }
+        }
+        return ans;
+        
+        
+    }
+}
