ae1787279a7aa8143e20b8ef85ad638e72dbb625
MacBook <patelyogesh5040@gmail.com>
2021-05-20 23:08:41 +0530

    Leetcode102

--- Tree/Leetcode102.java
+++ Tree/Leetcode102.java
@@ -2,44 +2,47 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-import javax.swing.tree.TreeNode;
+//  Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 
 public class Leetcode102 {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new LinkedList<>();
         Queue<TreeNode> ans = new LinkedList<>();
-        if(root == null) return result;
+        if (root == null)
+            return result;
         ans.add(root);
-        while(!ans.isEmpty()){
+        while (!ans.isEmpty()) {
             int size = ans.size();
             List<Integer> currentLevel = new LinkedList<>();
-            for(int i = 0; i < size; i++){
+            for (int i = 0; i < size; i++) {
                 TreeNode current = ans.remove();
                 currentLevel.add(current.val);
-                if(current.left != null){
+                if (current.left != null) {
                     ans.add(current.left);
                 }
-                if(current.right != null){
+                if (current.right != null) {
                     ans.add(current.right);
                 }
             }
-            result.add(currentLevel); 
+            result.add(currentLevel);
         }
         return result;
     }
